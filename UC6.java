f21f058653df1318c2961e69345ef09eaf274748
SwathiRamanan <sr3638@srmist.edu.in>
2026-03-17 13:54:00 +0530

    UC6

---
+++ UC6.java
@@ -0,0 +1,56 @@
+/**
+ * OOPSBannerApp UC6 - OOPS Banner Application (Use Case 6)
+ * This version uses a modular approach with dedicated methods for each letter.
+ * * @author Developer
+ * @version 6.0
+ */
+public class UC6 {
+
+    public static void main(String[] args) {
+
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        for (int i = 0; i < oPattern.length; i++) {
+            // Using String.join for clean formatting
+            System.out.println(String.join(" ", oPattern[i], oPattern[i], pPattern[i], sPattern[i]));
+        }
+    }
+
+    public static String[] getOPattern() {
+        return new String[] {
+            "   ***    ",
+            " **   **  ",
+            "**     ** ",
+            "**     ** ",
+            "**     ** ",
+            " **   **  ",
+            "   ***    "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[] {
+            "****** ",
+            "**   **",
+            "**   **",
+            "****** ",
+            "** ",
+            "** ",
+            "** "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[] {
+            " *****  ",
+            "**      ",
+            "**      ",
+            " *****  ",
+            "         ** ",
+            "         ** ",
+            "   ******   "
+        };
+    }
+}
